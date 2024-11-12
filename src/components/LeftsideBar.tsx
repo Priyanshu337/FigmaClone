35af5ebaa8be73559f87a1f84cd8950c3ab547d6
Priyanshu <priyanshu@Priyanshus-MacBook-Pro.local>
2024-11-13 01:06:28 +0530

    first commit

--- src/components/LeftsideBar.tsx
+++ src/components/LeftsideBar.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const LeftsideBar = () => {
     return (
-        <section className='d-flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20'>
+        <section className='d-flex flex-col justify-center items-center border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20'
+            style={{ width: '7%' }}>
             <h1>Elements</h1>
         </section >
     )
 }
 
-export default LeftsideBar
+export default LeftsideBar  
--- src/components/RightsideBar.tsx
+++ src/components/RightsideBar.tsx
@@ -5,9 +5,10 @@ const RightsideBar = () => {
         <section className='d-flex flex-col border-t  min-2-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20' style={{
             borderColor: '#d1d5db', // border-primary-grey-200
             backgroundColor: '#000000', // bg-primary-black
-            color: '#9ca3af', // text-primary-grey-300
+            color: 'grey', // text-primary-grey-300
             borderWidth: '1px', // Example border width, adjust as needed
             borderStyle: 'solid', // Example border style, adjust as needed
+            width: '7%'
         }}
         >
             <h1>Design</h1>
